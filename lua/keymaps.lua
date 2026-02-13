d5d68749ea8954ce17f4e0d89536071b343c4d4a
HaraldM <harald.magnusson24@gmail.com>
2026-02-13 08:12:49 +0100

    add wraparound to quickfix jumping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,8 +6,13 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.keymap.set("n", "<M-j>", "<cmd>cnext<cr>", { desc = "Quickfix: next item" })
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<cr>", { desc = "Quickfix: previous item" })
+vim.keymap.set("n", "<M-j>", "<cmd>try | cnext | catch | cfirst | catch | endtry<cr>", { desc = "Quickfix: next item" })
+vim.keymap.set(
+	"n",
+	"<M-k>",
+	"<cmd>try | cprev | catch | clast | catch | endtry<cr>",
+	{ desc = "Quickfix: previous item" }
+)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
